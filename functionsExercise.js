72dc4bde2626ebce7cf1037616b9765107c5ab35
Crystal Brown <crystalkeithbrown@gmail.com>
2021-03-15 09:19:53 -0400

    strted functionExercise

---
+++ functionsExercise.js
@@ -0,0 +1,12 @@
+function fozzieBear(){
+    console.log(`Wocka Wocka`);
+}
+function beaker (speak){
+    console.log(`${speak} whaterver is passed`);
+    console.log(`${speak}Meep`);
+}
+function werewolf (a , b){
+    console.log(`${a}'s weapon of choice is a ${b}`);
+    if 
+
+}
